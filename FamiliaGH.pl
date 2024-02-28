98f752476cfa1c832e46372ba020f5445c953c4b
KanoN10 <57761619+KanoN10@users.noreply.github.com>
2024-02-28 13:53:21 -0400

    Add files via upload
    27/02/2024

---
+++ FamiliaGH.pl
@@ -0,0 +1,42 @@
+% toda la familia
+
+ronaldo.
+maria.
+juan.
+carlos.
+yessica.
+cristian.
+lucero.
+peter.
+luciano.
+alvaro.
+
+% todos los hombres
+esHombre(ronaldo).
+esHombre(juan).
+esHombre(carlos).
+esHombre(cristian).
+esHombre(peter).
+esHombre(luciano).
+esHombre(alvaro).
+
+% todas las mujeres
+esMujer(yessica).
+esMujer(maria).
+esMujer(lucero).
+
+esHijoOhija(ronaldo,juan).
+esHijoOhija(ronaldo,carlos).
+
+esHijoOhija(maria,carlos).
+esHijoOhija(maria,yessica).
+esHijoOhija(yessica,peter).
+esHijoOhija(carlos,peter).
+esHijoOhija(yessica,luciano).
+esHijoOhija(carlos,luciano).
+esHijoOhija(yessica,lucero).
+esHijoOhija(carlos,lucero).
+esHijoOhija(lucero,alvaro).
+esHijoOhija(cristian,alvaro).
+
+
