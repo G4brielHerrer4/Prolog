c744a36bd3198be040441306bf8054d16225b40d
KanoN10 <57761619+KanoN10@users.noreply.github.com>
2024-03-02 17:08:51 -0400

    Add files via upload

---
+++ universidad.pl
@@ -0,0 +1,27 @@
+% Átomos presentes - Gabriel Herrera
+atomo(mario).
+atomo(arturica).
+atomo(andres).
+atomo(carla).
+atomo(magenta).
+atomo(giovana).
+atomo(elena).
+atomo(programacion1).
+atomo(redes2).
+atomo(ia).
+
+% Hechos presentes - Relaciones
+ensenia(mario, programacion1).
+ensenia(mario, redes2).
+ensenia(arturica, programacion1).
+ensenia(arturica, ia).
+ensenia(andres, redes2).
+
+pasa_clase(carla, redes2).
+pasa_clase(carla, ia).
+pasa_clase(magenta, programacion1).
+pasa_clase(magenta, ia).
+pasa_clase(magenta, redes2).
+pasa_clase(giovana, redes2).
+pasa_clase(giovana, ia).
+pasa_clase(elena, ia).
