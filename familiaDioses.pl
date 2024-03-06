206971144f02a14f0b12bab1c002a884446324e5
G4brielHerrer4 <gabo.gh2875@gmail.com>
2024-03-06 14:24:37 -0400

    cambio

---
+++ familiaDioses.pl
@@ -0,0 +1,30 @@
+%Hechos - Gabriel Herrera
+
+esHombre(cronos).
+esHombre(ares).
+esHombre(poseidon).
+esHombre(hefesto).
+esHombre(zeus).
+esHombre(baco).
+esHombre(hermes).
+
+esMujer(hera).
+esMujer(afrodita).
+esMujer(hipolita).
+esMujer(atena).
+esMujer(nera).
+
+esDescendienteDirecto(hera, cronos).
+esDescendienteDirecto(afrodita, ares).
+esDescendienteDirecto(afrodita, hera).
+esDescendienteDirecto(hefesto, ares).
+esDescendienteDirecto(hefesto, hera).
+esDescendienteDirecto(zeus, poseidon).
+esDescendienteDirecto(zeus, afrodita).
+esDescendienteDirecto(baco, afrodita).
+esDescendienteDirecto(baco, hefesto).
+esDescendienteDirecto(atena, afrodita).
+esDescendienteDirecto(atena, hefesto).
+esDescendienteDirecto(nera, hipolita).
+esDescendienteDirecto(nera, zeus).
+esDescendienteDirecto(hermes, atena).
