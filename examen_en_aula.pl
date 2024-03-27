76ee495b47b16fe394b77fb03f57ca5b124b83ff
G4brielHerrer4 <gabo.gh2875@gmail.com>
2024-03-26 20:41:25 -0400

    v2

--- examen_en_aula.pl
+++ examen_en_aula.pl
@@ -3,6 +3,11 @@ esProducto("Samsung B125").
 esProducto("LG Batimix").
 esProducto("Samsung L200").
 
+%2da parte
+tieneCaracteristica("LG-510", blanco).
+tieneCaracteristica("LG-510", barato).
+tieneCaracteristica("LG-510", bueno).
+
 esProducto("Bose",parlantes).
 esProducto("LG",parlantes).
 
@@ -32,3 +37,6 @@ estaCategoria("LG",parlantes).
 
 productoDentroCategoria(Producto, Categoria):- esCategoria(Categoria),esProducto(Producto), estaCategoria(Producto,Categoria).
 productoDentroCategoria(Producto, Categoria):- estaCategoria(Producto, Categoria),productoDentroCategoria(Producto,Categoria).
+
+recomendarMaria1(Producto):- esProducto(Producto),tieneCaracteristica(Producto,Verde),productoDentroCategoria(Producto,producto).
+recomendarMaria2(Producto):- esProducto(Producto),esVerde(Producto),productoDentroCategoria(Producto,producto).
